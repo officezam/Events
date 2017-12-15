c3f54fa99986ae916f266885cb9ca1a4df1fc810
officezam <officezam@gmail.com>
2017-12-15 22:11:18 +0500

    Mail Simple test

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -117,8 +117,17 @@ class MemberController extends Controller
 
 
 	public function TestEmailSending(){
-		$UserData   = User::find(5);
+		$UserData   = User::find(4);
 		$response = $this->sendEmail($UserData);
+
+		$to = "officezam@gmail.com";
+		$subject = "My subject";
+		$txt = "Hello world!";
+		$headers = "From: joinmyseo@gmail.com" . "\r\n" .
+		           "CC: joinmyseo@gmail.com";
+
+		$resp = mail($to,$subject,$txt,$headers);
+		dump($resp);
 		dd($response);
 	}
 	public function sendEmail($thisUser){
